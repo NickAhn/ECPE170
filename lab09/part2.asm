2964466c3fba82c6d18b11422f24015905f5d673
Nick Ahn <n_ahn2@u.pacific.edu>
2022-04-11 20:42:50 -0700

    lab 9 part 2

--- lab09/part2.asm
+++ lab09/part2.asm
@@ -9,6 +9,55 @@
 
 # The label 'main' represents the starting point
 main:
+
+    lw $s0, A # Load A to s0
+    lw $s1, B # Load B to s1
+    lw $s2, C # Load C to s2
+
+    #if(A>B || C < 5)
+    bge $s0, $s1, if # if A > B jump to if. Or
+    blt $s2, 5, if # if C < 5 jump to if
+
+    # else if((A > B) && ((C+1) == 7))
+    elif:
+        ble $s0, $s1, else # if A <= B, jump to else
+        addi $t1, $s2, 1 #adding 1 to C
+        bne $t1, 7, else # if ~(A == 7), jump to else
+        li $t0, 2 # Load immediate 2 to t0
+    j endif # end if statement
+
+    else:
+        li $t0, 3 # Load immediate 3 to $t0
+        j endif # end if statement
+        
+    if:
+        li $t0, 1 # Load 1 to t0
+        j endif # end if statement
+
+
+    endif:    
+
+    #Switch Case
+    beq $t0, 1, one # if t0 == 0
+    beq $t0, 2, two # if t0 == 2
+
+    #default
+    li $t0, 0 # Z = 0
+    j done # jump to done
+
+    # case 1
+    one:
+    li $t0, -1 # Z = -1
+    j done # jump to done
+
+    # case 2
+    two: 
+    li $t0, -2 # Z = -2
+    j done # jump to done
+
+    #done with Switch/case statements. Assign t0 to Z and end program
+    done: 
+    sw $t0, Z
 	# Exit the program by means of a syscall.
 	# There are many syscalls - pick the desired one
 	# by placing its code in $v0. The code for exit is "10"
